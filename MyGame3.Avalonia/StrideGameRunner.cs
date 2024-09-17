5b70f5b5303d9d303aab718218aa3af496b56235
Doprez <73259914+Doprez@users.noreply.github.com>
2024-09-17 16:43:45 -0600

    added separate thread game running?

---
+++ MyGame3.Avalonia/StrideGameRunner.cs
@@ -0,0 +1,42 @@
+﻿using Stride.Engine;
+using Stride.Games;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyGame3.Avalonia;
+public class StrideGameRunner
+{
+	private Thread gameThread;
+	private bool isRunning;
+	private GameBase gameInstance;
+	private GameContext gameContext;
+
+	public StrideGameRunner(GameBase game, GameContext context)
+	{
+		gameContext = context;
+		gameInstance = game;
+		isRunning = true;
+		gameThread = new Thread(GameLoop)
+		{
+			IsBackground = true,
+			Name = "StrideGameThread"
+		};
+		gameThread.Start();
+	}
+
+	private void GameLoop()
+	{
+		// Update and render the game
+		gameInstance.Run(gameContext);
+	}
+
+	public void Stop()
+	{
+		isRunning = false;
+		gameThread.Join();
+	}
+}
